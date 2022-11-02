778c84238f9a447a7c2d734bb9a84e7c8ff3875f
KonstantinStanmeyer <konstantin@stanmeyer.com>
2022-11-01 20:18:59 -0600

    closest to working yet, included wrap_params

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-class Users::RegistrationsController < Devise::RegistrationsController
+class Users::RegistrationsController < Devise::RegistrationsController 
+  wrap_parameters format: []
   respond_to :json
 
   private
@@ -13,7 +14,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
       }
     else
       render json: {
-        status: {message: "User couldn't be created successfully. #{resource.errors.full_messages.to_sentence}"}
+        status: {message: "User couldn't be created successfully. #{resource.errors.full_messages}"}
       }, status: :unprocessable_entity
     end
   end

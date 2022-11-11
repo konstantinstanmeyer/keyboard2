cf15e81bf89d0eb336d3f41272b9a465d823d910
KonstantinStanmeyer <konstantin@stanmeyer.com>
2022-11-10 22:37:05 -0700

    changing serializers for leaderboard

--- app/serializers/user_score_serializer_serializer.rb
+++ app/serializers/user_score_serializer_serializer.rb
@@ -1,5 +1,5 @@
 class UserScoreSerializerSerializer < ActiveModel::Serializer
-  attributes :id, :high_score, :username, :avatar_url
+  attributes :id, :high_score, :username, :avatar_url, :view_profile?, :view_high_score?
 
   has_many :scores
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
 
   attributes :id, :email, :password, :high_score, :avatar_url, :view_origin?, :username, :view_profile?, :view_high_score?
+
+  has_many :scores
 end

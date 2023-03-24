b67f9160e8bf31799f0071de774368c7bc2b4a32
konstantinstanmeyer <78387837+konstantinstanmeyer@users.noreply.github.com>
2023-03-23 19:56:47 -0600

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
 
+    # setup allowed parameters for devise gem | added avatar for activeStorage
     def configure_permitted_parameters    
         devise_parameter_sanitizer.permit { |u| u.permit(:avatar, :email, :password, :password_confirmation, :current_password) } 
     end

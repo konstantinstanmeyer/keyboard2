1e0559e09914fdaa07cccdc90b0ce63b570e481c
konstantinstanmeyer <78387837+konstantinstanmeyer@users.noreply.github.com>
2023-03-23 20:06:44 -0600

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 require 'pry'
 
 class User < ApplicationRecord
+  # allows file to be attached to a user within the avatar key's value | using activeStorage
+  # establish one-to-many relationship for user -> scores
   has_one_attached :avatar
   has_many :scores
   include Devise::JWT::RevocationStrategies::JTIMatcher
@@ -10,6 +12,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 
+  # grabs a user's profile picture is the value is present within its instance
   def avatar_url
     Rails.application.routes.url_helpers.url_for(avatar) if avatar.attached?
   end

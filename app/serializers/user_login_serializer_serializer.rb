bae5c23c51478b4181d05124d2e85a0ad7f74239
konstantinstanmeyer <78387837+konstantinstanmeyer@users.noreply.github.com>
2023-03-23 20:16:30 -0600

    Update user_login_serializer_serializer.rb

--- app/serializers/user_login_serializer_serializer.rb
+++ app/serializers/user_login_serializer_serializer.rb
@@ -1,4 +1,5 @@
 class UserLoginSerializerSerializer
+  # FastJsonapi currently functional for activeStorage blob queries
   include FastJsonapi::ObjectSerializer
   
   attributes :id, :email, :password, :high_score, :avatar_url
